b067ef83121a77fcd209613865081d3ae2599843
eqs <murashige.satoshi.mi1@is.naist.jp>
2021-02-24 15:56:49 +0900

    Update build config

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
-msbuild pyoflib\pyoflib.sln /t:clean;rebuild /p:Configuration=Release;Platform="x64"
+rem msbuild pyoflib\pyoflib.sln /t:clean;rebuild /p:Configuration=Release;Platform="x64"
+ msbuild pyoflib\pyoflib.sln /t:build /p:Configuration=Release;Platform="x64"
 if %ERRORLEVEL% neq 0 (
     echo ErrorLevel:%ERRORLEVEL%
     echo ビルド失敗
+    exit /b 1
 )
+
+exit /b 0
